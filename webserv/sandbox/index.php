8a8d6a0b8c10e8a0e7ee8b1823c1e5fc33a0c156
Rasmus <r.serrestam@gmail.com>
2022-09-15 18:16:15 +0200

    commit

--- webserv/sandbox/index.php
+++ webserv/sandbox/index.php
@@ -1,35 +1,55 @@
 <?php
-session_start();
-$_SESSION["TEST"] = array("hej", "vab", "daw");
+// lagra filnamnet i en variabel
+$filename = "test.txt";
 
-setcookie("hello", json_encode($_SESSION["TEST"]));
+// Nu gäller det att bestämma sig för vad du vill göra.
+// 1. Läs in hela filen och skriv ut den direkt
+// readfile tar som argument filnamnet
+readfile($filename);
+echo "<hr>";
 
+// 2. Skriv ut den rad för rad
+// Skapa ett filhandtag
+// första argumentet är filnamnet
+// andra argumentet är hur filen skall öppnas, "r" innebär bara läsning
+$file = fopen($filename, "r");
+// Loopa igenom filen, så länge det finns fler rader
+// feof -  end of file in a file pointer
+while(!feof($file)){
+  // Skriv ut varje rad för sig.
+  echo fgets($file)."<br />";
+  // Om du vill jobba med
+}
+echo "<hr>";
 
-$data = json_decode($_COOKIE['hello'], true);
-
-
-foreach($data as $d){
-    echo "<p>".$d."</p>";
+// 3. Läs in varje rad till en array.
+// Läser in filen igen eftersom den lästes till slutet i förra loopen
+$file = fopen($filename, "r");
+while(!feof($file)){
+  $arr[] = fgets($file);
 }
 
+// Loopar igenom hela arrayen
+foreach ($arr as $line) {
+  // Skriv ut varje rad för sig.
+  echo "$line<br />";
+}
+echo "<hr>";
+
+// 4. Läser in filen till två arrayer, course och desc
+$course = array(); $desc = array();
+// Läser in filen igen eftersom den lästes till slutet i förra loopen
+$file = fopen($filename, "r");
+while(!feof($file)){
+  $data = fgets($file);    // Läs rad till $data
+  if(strlen($data)>0){    // Om $data har innehåll
+    // Använd explode för att dela upp innehåll i olika arrayer
+    list($course[], $desc[]) = explode("|", $data);
+  }
+}
+
+// Skriv ut innehållet från våra två arrayer.
+for($i=0; $i<count($course); $i++){
+  echo "<p>Kurs: $course[$i]<br>Beskrivning: $desc[$i]</p>";
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-
-
-<pre>
-<?php
-  print_r(get_defined_vars());
- ?>
-</pre>
-</body>
-</html>
